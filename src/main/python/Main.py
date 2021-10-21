6160366dbdfbe58ddcecf4c213a0e1fd2156d8e5
Riccardo Romagnoli <riccardo.romagnoli.1998@gmail.com>
2021-10-21 20:58:58 +0200

    Recommendation

--- src/main/python/Main.py
+++ src/main/python/Main.py
@@ -1,4 +1,46 @@
 #!/usr/bin/python
 import sys
+import pandas as pd
+from rake_nltk import Rake
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+
 for line in sys.stdin:
     print("python script:" + line.upper())
+//TODO have df with index col titles and feature bag_of_words
+
+# instantiating and generating the count matrix
+count = CountVectorizer()
+count_matrix = count.fit_transform(df['bag_of_words'])
+
+# generating the cosine similarity matrix
+cosine_sim = cosine_similarity(count_matrix, count_matrix)
+
+# creating a Series for the movie titles so they are associated to an ordered numerical
+# list I will use in the function to match the indexes
+indices = pd.Series(df.index)
+
+#  defining the function that takes in movie title
+# as input and returns the top 10 recommended movies
+def recommendations(title, cosine_sim = cosine_sim):
+
+    # initializing the empty list of recommended movies
+    recommended_movies = []
+
+    # gettin the index of the movie that matches the title
+    idx = indices[indices == title].index[0]
+
+    # creating a Series with the similarity scores in descending order
+    score_series = pd.Series(cosine_sim[idx]).sort_values(ascending = False)
+
+    # getting the indexes of the 10 most similar movies
+    top_10_indexes = list(score_series.iloc[1:11].index)
+
+    # populating the list with the titles of the best 10 matching movies
+    for i in top_10_indexes:
+        recommended_movies.append(list(df.index)[i])
+
+    return recommended_movies
+
+ recommendations('The Strangers ')
